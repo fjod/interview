bcdcb2e6d1f1c28db78c6f07fc0ebce248dd00d1
fjod <fourjustoneday@yandex.ru>
2021-01-25 14:13:11 +0300

    oop push

--- AkinshinProblemBooks/AkinshinProblemBooks/OOP.cs
+++ AkinshinProblemBooks/AkinshinProblemBooks/OOP.cs
@@ -1,7 +1,65 @@
-﻿namespace AkinshinProblemBooks
+﻿using System;
+
+namespace AkinshinProblemBooks
 {
-    public class OOP
+    class Foo
     {
-        
+        protected class Quux
+        {
+            public Quux()
+            {
+                Console.WriteLine("Foo.Quux()");
+            }
+        }
+    }
+    class Bar : Foo
+    {
+        new class Quux //it's private, so when parent instantiated, this wont be overridder
+        {
+            public Quux()
+            {
+                Console.WriteLine("Bar.Quux()");
+            }
+        }
+    }
+    class Baz : Bar
+    {
+        public Baz()
+        {
+            new Quux();
+        }
+    }
+    
+    //Классы Foo<int> и Foo<double> — это два разных класса, у каждого из них собственное статитческое поле Bar.
+    class Foo<T>
+    {
+        public static int Bar;
+    }
+    
+    
+    public struct Foo2
+    {
+        public int Value;
+        public void Change(int newValue)
+        {
+            Value = newValue;
+        }
+    }
+    public class Bar2
+    {
+        //При обращении к свойству bar.Foo вызовется метод bar.get_Foo(), который вернёт нам копию структуры,
+        //для которой мы выполним метод Change
+        public Foo2 Foo { get; set; }
+    }
+    
+    struct Foo3
+    {   
+        int value;
+        public override string ToString()
+        { 
+            if (value == 2)
+                return "Baz";
+            return (value++ == 0) ? "Foo" : "Bar";
+        }
     }
 }
--- AkinshinProblemBooks/AkinshinProblemBooks/Program.cs
+++ AkinshinProblemBooks/AkinshinProblemBooks/Program.cs
@@ -10,9 +10,27 @@ namespace AkinshinProblemBooks
             Console.WriteLine("Hello World!");
             // AugmentedAssignment t = new AugmentedAssignment();
             // t.Main();
-
-            Rounding2 t = new Rounding2();
-            t.Work();
+            //
+            // Rounding2 t = new Rounding2();
+            // t.Work();
+            //new Baz();
+            //Foo<int>.Bar++;
+            //Console.WriteLine(Foo<double>.Bar);
+            
+            // var bar = new Bar2 { Foo = new Foo2() };
+            // bar.Foo.Change(5);
+            // Console.WriteLine(bar.Foo.Value);
+            
+            var foo = new Foo3();
+            Console.WriteLine(foo); //boxing, so struct is copied, so value is not changed
+            Console.WriteLine(foo);
+            object bar = foo;
+            object qux = foo;
+            object baz = bar;
+            Console.WriteLine(baz);
+            Console.WriteLine(bar);
+            Console.WriteLine(baz);
+            Console.WriteLine(qux);
         }
     }
 }
