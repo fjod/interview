141c7b0612f84717489173bc3fea08ce91549831
fjod <fourjustoneday@yandex.ru>
2021-01-27 15:47:03 +0300

    simple parallel webdownload

---
+++ TAP/FSharp/TaskStudy/HttpGetter.fs
@@ -0,0 +1,17 @@
+﻿module TaskStudy.HttpGetter
+
+open System
+open System.Net.Http
+
+let client = new HttpClient()
+let downloadString (uri:string)  =
+    let GoodUri = new Uri(uri)
+    async{
+       try
+        let! data = client.GetStringAsync(GoodUri) |> Async.AwaitTask
+        return Some data    
+       with
+       | :? System.Exception  -> return None        
+       
+    }
+    
--- TAP/FSharp/TaskStudy/Program.fs
+++ TAP/FSharp/TaskStudy/Program.fs
@@ -1,7 +1,9 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System
+open TaskStudy
 open TaskStudy.Basic
 open TaskStudy.Sleep
-
+open TaskStudy.HttpGetter
 // Define a function to construct a message to print
 
 
@@ -22,14 +24,26 @@ let main argv =
 //   printHello |> Async.Start
 //   FiveSecondsSleep
    
-   let rand = System.Random()
-   let pickNumAsync = async { return rand.Next(10) }
-   let create =
-       let workFlows = [1..50] |> Seq.map (fun _ -> pickNumAsync)
-       async{
-           let! numbs = workFlows |> Async.Parallel
-           printf "%d" (numbs |> Array.sum)
-       }
-   create |> Async.Start
-   FiveSecondsSleep
+//   let rand = System.Random()
+//   let pickNumAsync = async { return rand.Next(10) }
+//   let create =
+//       let workFlows = [1..50] |> Seq.map (fun _ -> pickNumAsync)
+//       async{
+//           let! numbs = workFlows |> Async.Parallel
+//           printf "%d" (numbs |> Array.sum)
+//       }
+//   create |> Async.Start
+//   FiveSecondsSleep
+   
+   ["http://www.fsharp.org"; "http://microsoft.com"; "http://fsharpforfunandprofit1.com"; "http://fsharpforfunandprofit.com"]
+   |> List.map downloadString
+   |> Async.Parallel
+   |> Async.RunSynchronously //this line waits for all tasks to finish
+   |> Array.map (fun i ->
+                 match i with
+                 | Some i -> i.Length.ToString()
+                 | _ -> "some error happened"
+                 )
+   |> Array.iter Console.WriteLine
+    
    0 // return an integer exit code
