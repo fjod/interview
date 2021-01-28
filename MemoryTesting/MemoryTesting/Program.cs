bd7c0d11311ea15f7b48021361cc92a67e6fd26d
fjod <fourjustoneday@yandex.ru>
2021-01-28 16:55:18 +0300

    small project to test dotMemory

---
+++ MemoryTesting/MemoryTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace MemoryTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i = 0;
+            while (i < 100000)
+            {
+                var range = Enumerable.Range(0, 10000);
+                List<int> test = new List<int>();
+                foreach (var i1 in range)
+                {
+                    test.Add(i1);
+                }
+                Console.Write(test.Sum() + " ");
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(300);
+                i++;
+            }
+
+            
+        }
+    }
+}
